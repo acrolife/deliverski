a4e11b99d64fdfb93eb0101d27774cdfa2f24bc4
funkmatch <david.dedobbeleer@creativityquarks.com>
2022-04-17 12:55:24 +0200

    quick hack was to implement this here after for the routes generating this behavior: when a the path "en/verify-email?t=5366920166083824" is targetted, the router send it to "undefined/en/verify-email?t=5366920166083824"

--- src/containers/AuthenticationPage/AuthenticationPage.js
+++ src/containers/AuthenticationPage/AuthenticationPage.js
@@ -96,10 +96,36 @@ export class AuthenticationPageComponent extends Component {
     const locationFrom = location.state && location.state.from ? location.state.from : null;
     const authinfoFrom =
       this.state.authInfo && this.state.authInfo.from ? this.state.authInfo.from : null;
-    const from = locationFrom ? locationFrom : authinfoFrom ? authinfoFrom : null;
+    let from = locationFrom ? locationFrom : authinfoFrom ? authinfoFrom : null;
 
     const user = ensureCurrentUser(currentUser);
     const currentUserLoaded = !!user.id;
+
+    // DEV
+    // console.log("hello login")
+    // console.log("from before", from)
+    // test with this in the console :
+    // let from = "undefined/en/verify-email?t=5366920166083824"
+
+    // This has been implemented in the case of the multilangage feature implemented on the site, 
+    // when a the path "en/verify-email?t=5366920166083824" is targetted, the router send it to "undefined/en/verify-email?t=5366920166083824"
+    // So the quick hack was to implement this here after for the routes generating this behavior.
+    let fromNew
+    try {
+      if (from.includes("undefined")) {
+        const fromArray = from.split('/')
+        // If the path includes "undefined", we have to remove "undefined/en" or "undefined/fr", say the two first elements of the path
+        fromArray.splice(0,2)
+        fromNew = fromArray.join('/')        
+      }
+    } catch (e) {
+      console.log(`Error @ Login: ${e}`)
+    } 
+
+    from = fromNew ? fromNew : from
+
+    // DEV
+    // console.log("from after", from)
 
     // We only want to show the email verification dialog in the signup
     // tab if the user isn't being redirected somewhere else
